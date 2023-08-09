486b17a84d61a778c102650d74da654b51486890
fede-at-616462008038 <cPw/21Olg2jWc8Pa4pSGalpyoiVYcBamfLYmAGb9FGo=>
2023-08-09 19:48:59 +0200

    Add logging

--- src/main/java/com/challenge/mobilemanagement/api/BookPhoneController.java
+++ src/main/java/com/challenge/mobilemanagement/api/BookPhoneController.java
@@ -2,8 +2,11 @@ package com.challenge.mobilemanagement.api;
 
 import com.challenge.mobilemanagement.api.requests.BookPhoneRequest;
 import com.challenge.mobilemanagement.api.responses.ResultResponse;
+import com.challenge.mobilemanagement.domain.Result;
 import com.challenge.mobilemanagement.usecases.bookPhone.BookPhoneCommand;
 import com.challenge.mobilemanagement.usecases.bookPhone.BookPhoneCommandHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +17,8 @@ import reactor.core.publisher.Mono;
 @RestController
 public class BookPhoneController {
 
+    public static final String PLEASE_CONTACT_OUR_TEAM = "Please contact our team";
+    Logger logger = LoggerFactory.getLogger(BookPhoneController.class);
     private final BookPhoneCommandHandler bookPhoneCommandHandler;
 
     public BookPhoneController(BookPhoneCommandHandler bookPhoneCommandHandler) {
@@ -24,10 +29,19 @@ public class BookPhoneController {
     public Mono<ResponseEntity<ResultResponse>> create(@RequestBody BookPhoneRequest bookPhoneRequest) {
 
         return bookPhoneCommandHandler.handle(BookPhoneCommand.fromRequest(bookPhoneRequest))
-                .flatMap(result -> {
-                    if (result.isSuccessful()) return Mono.just(ResponseEntity.ok().build());
-                    return Mono.just(ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON)
-                            .body(new ResultResponse(result.message())));
-                });
+                .flatMap(BookPhoneController::buildResponse)
+                .doOnError(error -> logger.error(error.getMessage()))
+                .onErrorReturn(handleUnknownError());
+    }
+
+    private static ResponseEntity<ResultResponse> handleUnknownError() {
+        return ResponseEntity.internalServerError().contentType(MediaType.APPLICATION_JSON)
+                .body(new ResultResponse(PLEASE_CONTACT_OUR_TEAM));
+    }
+
+    private static Mono<ResponseEntity<ResultResponse>> buildResponse(Result result) {
+        if (result.isSuccessful()) return Mono.just(ResponseEntity.ok().build());
+        return Mono.just(ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON)
+                .body(new ResultResponse(result.message())));
     }
 }
--- src/main/java/com/challenge/mobilemanagement/api/ReturnPhoneController.java
+++ src/main/java/com/challenge/mobilemanagement/api/ReturnPhoneController.java
@@ -2,8 +2,11 @@ package com.challenge.mobilemanagement.api;
 
 import com.challenge.mobilemanagement.api.requests.ReturnPhoneRequest;
 import com.challenge.mobilemanagement.api.responses.ResultResponse;
+import com.challenge.mobilemanagement.domain.Result;
 import com.challenge.mobilemanagement.usecases.returnPhone.ReturnPhoneCommand;
 import com.challenge.mobilemanagement.usecases.returnPhone.ReturnPhoneCommandHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +17,8 @@ import reactor.core.publisher.Mono;
 @RestController
 public class ReturnPhoneController {
 
+    public static final String PLEASE_CONTACT_OUR_TEAM = "Please contact our team";
+    Logger logger = LoggerFactory.getLogger(BookPhoneController.class);
     private final ReturnPhoneCommandHandler returnPhoneCommandHandler;
 
     public ReturnPhoneController(ReturnPhoneCommandHandler returnPhoneCommandHandler) {
@@ -23,10 +28,19 @@ public class ReturnPhoneController {
     @PostMapping("/return")
     public Mono<ResponseEntity<ResultResponse>> create(@RequestBody ReturnPhoneRequest returnPhoneRequest) {
         return returnPhoneCommandHandler.handle(ReturnPhoneCommand.fromRequest(returnPhoneRequest))
-                .flatMap(result -> {
-                    if (result.isSuccessful()) return Mono.just(ResponseEntity.ok().build());
-                    return Mono.just(ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON)
-                            .body(new ResultResponse(result.message())));
-                });
+                .flatMap(ReturnPhoneController::buildResponse)
+                .doOnError(error -> logger.error(error.getMessage()))
+                .onErrorReturn(handleUnknownError());
+    }
+
+    private static ResponseEntity<ResultResponse> handleUnknownError() {
+        return ResponseEntity.internalServerError().contentType(MediaType.APPLICATION_JSON)
+                .body(new ResultResponse(PLEASE_CONTACT_OUR_TEAM));
+    }
+
+    private static Mono<ResponseEntity<ResultResponse>> buildResponse(Result result) {
+        if (result.isSuccessful()) return Mono.just(ResponseEntity.ok().build());
+        return Mono.just(ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON)
+                .body(new ResultResponse(result.message())));
     }
 }
